f36408db287c45fcdbd9f2804445c802c55b362a
Andy Tran <andytran636@gmail.com>
2020-03-09 17:59:27 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>ANIMAL RESCUE</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/landing.css">
+</head>
+<body>
+	<div class="row splash">
+			<div class="col-xs-12">
+				<div id="content">
+					<h1>WELCOME TO ANIMAL RESCUE</h1>
+					<p>| AIDING THE MANAGEMENT OF OUR VERY OWN RESCUED AND RESCUEES TO THE BEST OF OUR PAW-BILITY SINCE 2019 |</p>
+					<div class="spacer"></div>
+					<hr>
+					<button class="btn btn-default btn-lg" onclick="location.href = 'pages/loginPage.php'"><i class="fa fa-address-card"></i> GET STARTED!</button>
+				</div>
+			</div>
+	</div>
+
+	<div class="container main">
+		<h1>WHO WE ARE</h1>
+		<p>Animal Rescue is a non-profit organization that shelters animals in need. Founded in 1982 by Ricky Rick, Animal Rescue has located forever homes for over 2000 furry friends! Our organization is proud to have our great team of volunteers who keep us running. At Animal Rescue we strive to treat the animals with utmost respect and try to keep them as comfortable and happy as they can be until they find their forever home.</p>
+		<h1>WHAT WE BELIEVE IN</h1>
+		<p>We believe that our furry pals here deserve to live their best lives. Our volunteers regularly clean and play with the animals to keep them healthy and social. Our experienced team of on-site veterinarians keep our animals healthy and are always on-call to assist with complication with animals who require a little more help than the others.</p>
+		<h1>OUR GOAL</h1>
+		<p>Our goal is to have the shelter as empty as possible at all times, making sure animals are perfect for their homes. We would love to have you come in and volunteer with us or potentially adopt a new furry friend!</p>
+	</div>
+
+	<div class="container info">
+		<div class="col-6">
+			
+		</div>
+		<div class="col-6">
+			<p>CONTACT US</p>
+			<p><i class="fa fa-home"></i> 56 Rescue Drive NE, Calgary, AB, TJ4 5H2</p>
+			<p><i class="fa fa-phone-square"></i> (587) 678-0981</p>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
